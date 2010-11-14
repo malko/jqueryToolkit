a1557424861e9d68bbac535c3bccac0d062c9a25
malko <malko@113d30c4-b292-4fcb-822f-cdb5e9a56acc>
2010-11-14 00:03:42 +0000

    - many changes to splitpane, added storable options opened and size + make it work in quirk mode (disabling toggling animation)
    git-svn-id: http://svn.jgotti.net/jqueryToolkit/trunk@13 113d30c4-b292-4fcb-822f-cdb5e9a56acc

--- src/plugins/splitpane/jquery.tk.splitpane.js
+++ src/plugins/splitpane/jquery.tk.splitpane.js
@@ -1,13 +1,19 @@
 /**
-known bug doesn't work in ie with quirkMode
+plugin for splitPane implementation
+@author jonathan gotti <jgotti at jgotti dot net>
+@licence Dual licensed under the MIT / GPL licenses.
+@since 2010-11
 */
 (function(){
-
 $.toolkit('tk.paneSplitter',{
 	_classNameOptions:{
 		 min:"|\\d+"
 		,max:"|\\d+"
-		//- ,opened:"|opened|closed"
+		,opened:"|opened|closed"
+		,size:"|\\d+"
+	},
+	_storableOptions:{
+		urlElementLevel:'opened|size'
 	},
 	_init:function(){
 		var self = this;
@@ -28,31 +34,37 @@ $.toolkit('tk.paneSplitter',{
 			top:0,
 			left:0
 		});
-		self._applyOpts('orientation|primary|min|max');
+		self._applyOpts('orientation|primary|min|max|opened|size|toggleSpeed');
 		self._tk.initialized=true;
 		self.__checkSize(true);
 	},
 	/** setters */
 	_set_orientation:function(o){
 		$.toolkit._removeClassExp(this.elmt,this._tk.baseClass+'-orientation-*',this._tk.baseClass+'-orientation-'+o);
-		this.elmt.height(o==='horizontal'?this.elmt.parent().height():'');
+		//- this.elmt.css({'height':'','width':''});
 		this.options.orientation = o;
 		this.__checkSize(true);
 	},
 	_set_primary:function(p){
+		var opened = this.options.opened, paneOpts, i;
+		if( this._tk.initialized ){ //-- be sure to not let old primary in a closed state
+			this.set('opened',true);
+		}
 		this.primary = this[p];
 		this.secondary = this[p==='prev'?'next':'prev'];
-		var paneOpts = $.toolkit._readClassNameOpts(this.secondary,$.tk.splitpane._tk.baseClass+'-pane',this._classNameOptions)
-			,i;
-		for( i in this._classNameOptions){
-			if( paneOpts[i] ){
-				this.set(i,paneOpts[i]);
-			}
+		paneOpts = $.toolkit._readClassNameOpts(this.secondary,$.tk.splitpane._tk.baseClass+'-pane',this._classNameOptions);
+		//-- don't override stored options
+		if( paneOpts.opened && opened!==null){
+			delete paneOpts.opened;
 		}
-		if( paneOpts.min ){
-			this.set('min',paneOpts.min)
+		paneOpts.size = this.options.size?this.options.size:(paneOpts.size?paneOpts.size:0);
+		this.options.primary = p;
+		this.set(paneOpts);
+		if(this._tk.initialized){ // restore opened state
+			this.set('opened',opened);
 		}
 		this.__checkSize(true);
+		return p;
 	},
 	_set_min:function(m){
 		this.options.min = Math.max(0,parseInt(m,10));
@@ -65,48 +77,80 @@ $.toolkit('tk.paneSplitter',{
 		return this.options.max;
 	},
 
-	/** accessor to primary and secondary panes elemnts * /
-	,getSecondary:function(returnElmt){
-		var s = (this.options.primary==='prev')?'next':'prev';
-		return returnElmt?this[s]:s;
-	}
-	,getPrimary:function(returnElmt){
-		return returnElmt?this[this.options.primary]:this.options.primary;
-	}//*/
-
-	/** toggle callback */
-	toggle:function(e){
+	_set_size:function(s){
+		this.secondary[this.__sizeProp()](s);
+		this.__resized();
+	},
+	_set_toggleSpeed:function(s){
+		if(! $.support.boxModel ){
+			return 0;
+		}
+		return typeof s==='number'?s:parseInt(s,10);
+	},
+	_set_opened:function(o){
+		//- console.trace()
+		if( typeof o !== 'boolean'){
+			o = (o==='closed'||o==='false')?false:true;
+		}
+		var secondaryVisible = this.secondary.is(':visible');
+		if( o === secondaryVisible ){
+			return o;
+		}
 		var self=this
-			, secondaryVisible = self.secondary.is(':visible')
+			//- , secondaryVisible = self.secondary.is(':visible')
+			, opened = self.options.opened
 			, sizeProperty = self.__sizeProp()
 			, sizeVal = self.secondary[sizeProperty]()
 			, outerSizeVal=self.secondary[self.__outerProp(sizeProperty)](true)
+			, initialized = self._tk.initialized?self.elmt.parent('.tk-splitpane').splitpane('get1_pluginInstance')._tk.initialized:false;
 			;
+		//-- get instance of parent splitpane
+		if( ! ( initialized && self.options.toggleSpeed > 0 ) ){//-- no anim at init time or quirks mode
+			if( self.primary.is(':visible') ){ //- adjust primary
+				self.primary[sizeProperty](self.primary[sizeProperty]()+(o?-outerSizeVal:outerSizeVal));
+			}
+			if( o ){
+				self.secondary[sizeProperty](sizeVal).show(0);
+				if( !$.support.boxModel){
+					self.secondary.css('display','');
+				}
+			}else{
+				self.secondary.hide(0)[sizeProperty](sizeVal);
+			}
+			self.__checkSize();
+			self.__resized();
+			return o;
+		}
 
 		if( self.primary.is(':visible') ){ //- adjust primary
-			var pSizeVal = (secondaryVisible?"+":"-")+"="+outerSizeVal;
+			var pSizeVal = (o?"-":"+")+"="+outerSizeVal;
 			pSizeVal = sizeProperty==='height'?{height:pSizeVal}:{width:pSizeVal};
 			self.primary.animate(pSizeVal,self.options.toggleSpeed,self.options.toggleEasing);
 		}
-
-		if( secondaryVisible ){ //-- hide secondary
-			self.secondary.animate(
-				sizeProperty==='height'?{height:0}:{width:0}
-				, self.options.toggleSpeed
-				, self.options.toggleEasing
-				, function(){$(this).hide()[sizeProperty](sizeVal);self.__resized();}
-			);
-		}else{ //-- show secondary
+		if(o){ //-- show secondary
 			self.secondary[sizeProperty](0).show().animate(
 				sizeProperty==='height'?{height:sizeVal}:{width:sizeVal}
 				, self.options.toggleSpeed
 				, self.options.toggleEasing
 				, function(){self.__resized();}
 			);
+		}else{ //-- hide secondary
+			self.secondary.animate(
+				sizeProperty==='height'?{height:0}:{width:0}
+				, self.options.toggleSpeed
+				, self.options.toggleEasing
+				, function(){$(this).hide()[sizeProperty](sizeVal);self.__resized();}
+			);
 		}
+		self.__checkSize();
 		self.__resized();
+		return o;
 	},
-
+	/** toggle callback */
+	toggle:function(e){
+		this.set('opened',!this.options.opened);
+		return this;
+	},
 	/** dragging separators */
 	startCapture:function(e){
 		e.preventDefault();
@@ -163,10 +207,12 @@ $.toolkit('tk.paneSplitter',{
 			return self.toggle(e);
 		}
 		self.clone.hide();
+		// check action
 		self.prev[sizeProp](self.prev[sizeProp]()+delta);
 		self.next[sizeProp](self.next[sizeProp]()-delta);
 		self.__checkSize();
-		self.__resized();
+		self.set('size',self.secondary[sizeProp]());
+		//self.__resized();
 	},
 
 	/** internals */
@@ -177,26 +223,25 @@ $.toolkit('tk.paneSplitter',{
 	__checkSize:function(triggerEvent){
 		var self = this;
 		if(! self._tk.initialized){
+		//- if(! self.secondary){
 			return;
 		}
-		//- dbg('check size',self.secondary,self.options.orientation,self.options.min)
-		if( self.secondary.is(':visible') ){
-			var sizeProp = self.__sizeProp(),sizeVal=self.secondary[sizeProp]();
+		//- if( self.secondary.is(':visible') ){
+			var sizeProp = self.__sizeProp(),sizeVal=self.secondary[sizeProp](),resized=false;
 			if( self.options.min && self.options.min > sizeVal ){
 				self.secondary[sizeProp](self.options.min);
 				self.primary[sizeProp](self.primary[sizeProp]()- (self.options.min - sizeVal));
-				if( triggerEvent ){
-					self.__resized();
-				}
+				resized=true;
 			}
 			if( self.options.max && self.options.max < sizeVal ){
 				self.secondary[sizeProp](self.options.max);
 				self.primary[sizeProp](self.primary[sizeProp]()+ (sizeVal-self.options.max));
-				if( triggerEvent ){
-					self.__resized();
-				}
+				resized=true;
 			}
-		}
+			if( resized && triggerEvent ){
+				self.__resized();
+			}
+		//- }
 	},
 	__get_capturePostitionDelta:function(e){
 		var posProp = this.__posProp();
@@ -217,9 +262,11 @@ $.tk.paneSplitter.defaults = {
 	primary:'next'
 	, orientation:'vertical'
 	, toggleEasing:'linear'
-	, toggleSpeed:100
+	, toggleSpeed:100 // put 0 for no anim (always 0 with quirks mode)
 	, min:0
 	, max:0
+	, opened:null
+	, size:0
 };
 
 
@@ -235,30 +282,35 @@ $.toolkit('tk.splitpane',{
 		self.panes = self.elmt.children('div').addClass(self._tk.baseClass+'-pane');
 		//-- make separators
 		self.panes.not(':last').after('<div class="'+$.tk.paneSplitter._tk.baseClass+'"></div>');
-		self.separators = self.elmt.find(' > .'+$.tk.paneSplitter._tk.baseClass).paneSplitter();
-		//-- make the last pane (if only 2 ) the main one, else make the one before a main pane
-		if( self.options.mainPane!==null && self.options.mainPane < self.panes.length){
-			self.mainPane = self.panes.filter(':eq('+self.options.mainPane+')');
-			if( self.options.mainPane < (self.panes.length-1) ){
-				self.mainPane.next('.'+$.tk.paneSplitter._tk.baseClass).paneSplitter('set_primary','prev');
-			}
-		}else if( self.separators.length>1){
-			self.separators.filter(':last').paneSplitter('set_primary','prev');
-			self.mainPane = self.panes.eq(self.panes.length-2);
-		}else{
-			self.mainPane = self.panes.filter(':last');
-		}
 		self._applyOpts('orientation');
+		self.separators = self.elmt.find(' > .'+$.tk.paneSplitter._tk.baseClass);
+		//-- determining the mainPane
+		if( self.options.mainPane===null || self.options.mainPane >= self.panes.length  || self.options.mainPane < 0){
+			self.options.mainPane = self.panes.length-(self.separators.length>1?2:1);
+		}
+		self.mainPane = self.panes.filter(':eq('+self.options.mainPane+')');
+		//-- instanciate splitters with correct options
+		if( self.options.mainPane === (self.panes.length-1)){
+			self.separators.paneSplitter({orientation:self.options.orientation});
+		}else{
+			var mainSplitter = self.mainPane.next('.'+$.tk.paneSplitter._tk.baseClass);
+			self.separators.not(mainSplitter).paneSplitter({orientation:self.options.orientation});
+			mainSplitter.paneSplitter({orientation:self.options.orientation,primary:'prev'});
+		}
+		//-- sizing elements
 		self.elmt.bind('resize',function(){ self.__resize(); });
+		self.__resize();
 	},
 	_set_mainPane:function(){
 		throw('mainPane can only be set at initialisation time in this version');
 	},
 	_set_orientation:function(o){
 		$.toolkit._removeClassExp(this.elmt,this._tk.baseClass+'-orientation-*',this._tk.baseClass+'-orientation-'+o);
-		this.separators.paneSplitter('set_orientation',o);
-		this.options.orientation = o;
-		this.__resize();
+		if( this.separators){
+			this.separators.paneSplitter('set_orientation',o);
+			this.options.orientation = o;
+			this.__resize();
+		}
 	},
 	__resize:function(){
 		var self = this;
@@ -266,15 +318,15 @@ $.toolkit('tk.splitpane',{
 			var h=self.elmt.height()-self.mainPane.outerHeight(true)+self.mainPane.height()
 				, w=self.elmt.width();
 			self.separators.each(function(){h-=$(this).width(w).outerHeight(true);});
-			self.panes.filter(':visible').each(function(){var e = $(this); e.width(w-e.outerWidth(true)+e.width())})
-				.not(self.mainPane).each(function(){h-=$(this).outerHeight(true);});
+			self.panes.each(function(){var e = $(this); e.width(w-e.outerWidth(true)+e.width())})
+				.filter(':visible').not(self.mainPane).each(function(){h-=$(this).outerHeight(true);});
 			self.mainPane.height(h);
 		}else{
 			var w=self.elmt.width()-self.mainPane.outerWidth(true)+self.mainPane.width()
 				, h=self.elmt.height();
 			self.separators.each(function(){w-=$(this).height(h).outerWidth(true)});
-			self.panes.filter(':visible').each(function(){var e = $(this); e.height(h-e.outerHeight(true)+e.height())})
-				.not(self.mainPane).each(function(){w-=$(this).outerWidth(true);});
+			self.panes.each(function(){var e = $(this); e.height(h-e.outerHeight(true)+e.height())})
+				.filter(':visible').not(self.mainPane).each(function(){w-=$(this).outerWidth(true);});
 			self.mainPane.width(w);
 		}
 	}
